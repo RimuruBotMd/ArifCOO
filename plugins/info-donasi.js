98b0daaa56e7abbd63c10113ab8ae929943d1b40
RifBotZ <105032230+RifBotZ@users.noreply.github.com>
2022-06-26 01:14:08 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -4,10 +4,13 @@ let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
 *✃ ᴘᴀʏᴍᴇɴᴛ*
+
 • _*Pulsa:*_ [089636219593]]
 • _*Dana:*_ [0895347198105]
 • _*Gopay:*_ [089636219593]
+
 ʂɛʈɛℓɑɦ ∂ɵɲɑʂɨ ӄɨɾɨɱ ɓựӄʈɨ ∂ɵɲɑʂɨ ӄɛ ɵωɲɛɾ
+TQ YOU WIBU
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/RifBotZ', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
