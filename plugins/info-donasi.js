51e29f22c9a954ddde876817c4d488763d52e177
RifBotZ <105032230+RifBotZ@users.noreply.github.com>
2022-06-26 01:03:07 +0700

    Update info-donasi.js

--- plugins/info-donasi.js
+++ plugins/info-donasi.js
@@ -3,10 +3,11 @@ let handler = async (m, { conn, command, usedPrefix }) => {
 let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
 let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
 let str = ` ${conn.getName(m.sender)} Want Support Bot?
-*PAYMENT ↓*
-_*Pulsa/pulse(Telkomsel):*_ 082287219167
-_*Dana/ovo:*_ 0895612153565
-Setelah melakukan donasi kirim bukti pembayaran ke owner
+*✃ ᴘᴀʏᴍᴇɴᴛ*
+• _*Pulsa:*_ [6289636219593]]
+• _*Dana:*_ [0895347198105]
+• _*Gopay:*_ [6289636219593]
+ʂɛʈɛℓɑɦ ∂ɵɲɑʂɨ ӄɨɾɨɱ ɓựӄʈɨ ∂ɵɲɑʂɨ ӄɛ ɵωɲɛɾ
 `
 conn.sendHydrated(m.chat, str, wm, pepe, 'https://github.com/FahriAdison/', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
 ['𝙼𝙴𝙽𝚄', '/menu']
